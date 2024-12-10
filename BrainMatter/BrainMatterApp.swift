bc9d47e384132fb31a10ddeccbe9bae44f19667b
JDeWeese <jdeweese574@icloud.com>
2024-12-10 10:46:22 -0600

    Initial Commit

---
+++ BrainMatter/BrainMatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainMatterApp.swift
+//  BrainMatter
+//
+//  Created by Joseph DeWeese on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainMatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
